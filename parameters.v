118684cec865b7a1c430c8f92e67064356b0b316
Hooman <hhoomn390@gmail.com>
2016-01-30 14:19:34 +0330

    params

---
+++ parameters.v
@@ -0,0 +1,18 @@
+`ifndef parameters
+`define parameters 1
+
+
+`define max_router 1024
+`define maxio 16
+`define maxvc 8
+`define crbufsz 100
+`define max_cr_delay 100
+
+
+`define NOP 0
+`define LoadStaging 1
+`define Phase0 2
+`define Phase1 3
+`define LoadRt 4
+`define Init   5
+`endif
