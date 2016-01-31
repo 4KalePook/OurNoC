916aaa586be2898536f6f14790083f6ba9031f0b
Mahdi <mbehroozikhah@gmail.com>
2016-01-31 22:51:48 +0330

    add struct

--- parameters.v
+++ parameters.v
@@ -19,21 +19,6 @@
 `define max_cr_delay 100
 `define max_cr_delay_bit 7
 
-`define BufferBitSize 22 //{full[1],vc[5],flit[16]}
-`define .BufferFull [21:21]
-`define .BufferVc [20:16]
-
-`define FlitSize 16 //{dst[14], head[1], tail[1]}
-`define .FlitDst [15:2]
-`define .FlitHead [1:1]
-`define .FlitTail [0:0]
-
-
-`define DataBitSize 32 //{dst[14], vc[5], num_flit[2]}
-`define .DataDst [31:18]
-`define .DataVc [17:13]
-`define .DataNumFlit [12:11]
-
 `define in_cycle_size 16
 `define op_size 3
 //TODO: mem_size should check
@@ -49,4 +34,30 @@
 `define CopyStaging 0
 `define State_bit 3
 `define Range(i,siz) siz*(i+1)-1:siz*i
+
+
+
+/*******************************
+**      Struct Buffer        **
+*******************************/
+`define BufferBitSize 22 //{full[1],vc[5],flit[16]}
+`define .BufferFull [21:21]
+`define .BufferVc [20:16]
+
+/*******************************
+**      Struct Flit           **
+*******************************/
+`define FlitSize 16 //{dst[14], head[1], tail[1]}
+`define .FlitDst [15:2]
+`define .FlitHead [1:1]
+`define .FlitTail [0:0]
+
+/*******************************
+**      Struct Data           **
+*******************************/
+`define DataBitSize 32 //{dst[14], vc[5], num_flit[2]}
+`define .DataDst [31:18]
+`define .DataVc [17:13]
+`define .DataNumFlit [12:11]
+
 `endif
